fbcae07c998b16eeeb723855e97330b60ce1f546
Vladimir Nagaev <nagaevvd1289@gmail.com>
2021-07-01 16:02:14 +0400

    is done

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -7,20 +7,18 @@ import javax.xml.parsers.SAXParser;
 import javax.xml.parsers.SAXParserFactory;
 import java.io.File;
 import java.io.IOException;
-import java.util.HashSet;
-import java.util.List;
 
-/**
- * Burn in Hell!!!!
- */
 public class App {
-    public static void main(@org.jetbrains.annotations.NotNull String[] args) throws ParserConfigurationException, SAXException, IOException {
-        Handler handler = new Handler();
-        File file = new File(args[0]);
-        SAXParserFactory parserFactory = SAXParserFactory.newInstance();
-        SAXParser parser = parserFactory.newSAXParser();
-        parser.parse(file, handler);
-//        System.out.println(handler.getUniqueCount());
-
+    public static void main(String[] args) throws ParserConfigurationException, SAXException, IOException {
+        if (args.length > 0) {
+            final Handler handler = new Handler();
+            final File file = new File(args[0]);
+            final SAXParserFactory parserFactory = SAXParserFactory.newInstance();
+            final SAXParser parser = parserFactory.newSAXParser();
+            parser.parse(file, handler);
+            System.out.println(handler.getUniqueCount());
+        } else {
+            System.out.println("XML file argument required");
+        }
     }
 }
--- src/main/java/org/example/Handler.java
+++ src/main/java/org/example/Handler.java
@@ -1,64 +1,23 @@
 package org.example;
 
 import org.xml.sax.Attributes;
-import org.xml.sax.SAXException;
 import org.xml.sax.helpers.DefaultHandler;
 
-import java.util.ArrayList;
 import java.util.HashSet;
-import java.util.List;
+import java.util.Set;
 
 public class Handler extends DefaultHandler {
+    private final Set<String> uniqueModifications = new HashSet<>();
 
-    private List<String> modificationNames = new ArrayList<String>();
-    private HashSet<String> uniqueModifications;
-
-    private String thisElement;
-    private  String thisAttribute;
-
-    public List<String> getNames() {
-        return modificationNames;
-    }
-
-    public HashSet<String> getUniqueModifications() {
-        return uniqueModifications;
-    }
     public int getUniqueCount() {
         return uniqueModifications.size();
     }
 
     @Override
-    public void startDocument() {
-        System.out.println("stat parsing...");
-    }
-
-    @Override
-    public void endDocument() {
-        System.out.println(uniqueModifications.size());
-        System.out.println("... end parsing");
-    }
-
-    @Override
-    public void startElement(String uri, String localName, String qName, Attributes attributes) throws SAXException {
-        thisElement = qName;
-        thisAttribute = attributes.getValue("name");
-    }
-
-    @Override
-    public void endElement(String namespace, String localName, String qname) throws SAXException {
-        thisElement = "";
-        thisAttribute = "";
-    }
-
-    @Override
-    public void characters(char[] chars, int st, int en) {
-        if (thisElement.equals("modification")) {
-            String modifName = new String(chars, st, en);
-            modificationNames.add(thisAttribute);
-            uniqueModifications.add(thisAttribute);
-            System.out.println(thisAttribute);
+    public void startElement(String uri, String localName, String qName, Attributes attributes) {
+        if (qName.equals("modification")) {
+            final String thisAttribute = attributes.getValue("name");
+            uniqueModifications.add(thisAttribute.trim());
         }
-
     }
-
 }
